5095d653076992560dd40db3a360cfc6ebb8e70c
Pratham shetty <prathamshetty484@gmail.com>
2024-01-29 17:22:54 +0530

    hello world

--- frontend/src/helpers/axiosInstance.js
+++ frontend/src/helpers/axiosInstance.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const BASE_URL="http://localhost:5014/api/v1";//url of server
+const BASE_URL="http://lms-server-6cnq.onrender.com/api/v1"
+//"http://localhost:5014/api/v1";//url of server
 const axiosInstance=axios.create();
 axiosInstance.defaults.baseURL=BASE_URL;
 axiosInstance.defaults.withCredentials=true;
